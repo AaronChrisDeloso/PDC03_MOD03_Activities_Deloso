09bbf496fdc0cc2be1a18fb8565135856b2ef6da
Aaron Chris Deloso <deloso.aaronchris@auf.edu.ph>
2022-05-09 20:50:54 +0800

    Add project files.

---
+++ PDC03_MOD03/PDC03_MOD03/Activity1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MOD03
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Activity1 : ContentPage
+    {
+        public Activity1()
+        {
+            InitializeComponent();
+        }
+        private async void Back1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+    }
+}
